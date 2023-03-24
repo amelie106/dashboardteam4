64ea2923a68e8699b1e4d1425e902c8f6d319f83
amelie106 <amelie.schneider106@gmail.com>
2023-03-24 15:47:39 +0100

    Refine cereal.py

--- cereal/cereal.py
+++ cereal/cereal.py
@@ -9,11 +9,10 @@ st.title("Real-Time / Live Data Science Dashboard")
 # top-level filters
 job_filter = st.selectbox("Select the Cereal", pd.unique(df["name"]))
 
-# creating a single-element container
-placeholder = st.empty()
-
 # dataframe filter
 df = df[df["name"] == job_filter]
 df = df.melt(id_vars='name')
+
 print(df)
+
 st.bar_chart(df, x='variable', y='value')
